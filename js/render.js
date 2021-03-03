3544c4d3e9a0e3889baae20369148b3eed36bc3a
pukmajster <45610204+pukmajster@users.noreply.github.com>
2021-03-03 16:24:42 +0100

    raiiiiin

--- js/render.js
+++ js/render.js
@@ -1,6 +1,11 @@
 function Root() {
     return html`
-    ${BackgroundImageUrl({ url: 'https://images.wallpapersden.com/image/download/night-mountains-summer-illustration_a2plamaUmZqaraWkpJRsa25trWloaGU.jpg'})}
+
+        <!-- https://tympanus.net/Development/RainEffect/index.html -->
+        ${config.rain && `<canvas width="1" height="1" id="container" style="position:absolute; z-index: -1"></canvas>`}
+
+
+        ${BackgroundImageUrl({ url: 'https://images.wallpapersden.com/image/download/night-mountains-summer-illustration_a2plamaUmZqaraWkpJRsa25trWloaGU.jpg'})}
         <section id="main"  class="animate__animated animate__fadeIn">
             
             <div id="Top" >
